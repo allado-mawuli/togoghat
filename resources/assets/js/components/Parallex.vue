0410bf2e193e03d1b6d1eda97803b64546749024
romuald <romualdlevent@gmail.com>
2021-01-20 16:55:44 +0530

    first commit

---
+++ resources/assets/js/components/Parallex.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-parallax
+    height="700"
+    src="https://assets.hongkiat.com/uploads/40-cool-abstract-and-background-photoshop-tutorials/yellow-green-abstract.jpg"
+  >
+  <v-layout
+      align-center
+      column
+      justify-center
+    >
+      <h1 class="display-4 font-weight-thin mb-3">UNIVERS</h1>
+      <h4 class="headline">One Platform For All</h4>
+    </v-layout>
+  </v-parallax>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
